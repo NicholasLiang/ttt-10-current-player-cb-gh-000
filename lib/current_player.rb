bd3267f47990612ffad7af25c7214b2a386a9872
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 07:49:18 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,17 @@
 def turn_count()
+  
+  
+  
+    it 'counts occupied positions' do
+      board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+
+      expect(turn_count(board)).to eq(3)
+    end
+  end
+
+  describe '#current_player' do
+    it 'returns the correct player, X, for the first move' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+      expect(current_player(board)).to eq("X")
+    end
